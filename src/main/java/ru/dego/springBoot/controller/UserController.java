7afd21a19349b7fe869ace0c52f0867a4b1f5ed4
Ruslan Kapnin <degogrand888@gmail.com>
2023-05-31 04:58:08 +0300

    Changed Controller to use UserService interface

--- src/main/java/ru/dego/springBoot/controller/UserController.java
+++ src/main/java/ru/dego/springBoot/controller/UserController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import ru.dego.springBoot.model.User;
-import ru.dego.springBoot.service.UserServiceImpl;
+import ru.dego.springBoot.service.UserService;
 
 import java.util.List;
 
@@ -14,11 +14,11 @@ import java.util.List;
 @RequestMapping(value = "/users")
 public class UserController {
     @Autowired
-    private UserServiceImpl userServiceImpl;
+    private UserService userService;
 
     @GetMapping()
     public String getUsers(ModelMap model) {
-        List<User> users = userServiceImpl.getUsers();
+        List<User> users = userService.getUsers();
         model.addAttribute("users", users);
         model.addAttribute("user", new User());
         return "users";
@@ -26,25 +26,25 @@ public class UserController {
 
     @GetMapping("/{id}/edit")
     public String getEditUser(ModelMap model, @PathVariable("id") Long id) {
-        model.addAttribute("user", userServiceImpl.getById(id));
+        model.addAttribute("user", userService.getById(id));
         return "editUser";
     }
 
     @PostMapping()
     public String addUser(@ModelAttribute("user") User user) {
-        userServiceImpl.save(user);
+        userService.save(user);
         return "redirect:/users";
     }
 
     @PatchMapping("/{id}")
     public String updateUser(@ModelAttribute("user") User user, @PathVariable("id") Long id) {
-        userServiceImpl.update(id, user);
+        userService.update(id, user);
         return "redirect:/users";
     }
 
     @DeleteMapping("/{id}")
     public String deleteUser(@PathVariable("id") Long id) {
-        userServiceImpl.remove(id);
+        userService.remove(id);
         return "redirect:/users";
     }
 }
